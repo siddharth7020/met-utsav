6746384aa135481e8fdce72776bfc1423f38fcc1
siddharth7020 <sidh1583@gmail.com>
2024-11-16 16:28:07 +0530

    prototype pages added

--- client/src/utils/login/ForgotPassword.jsx
+++
@@ -1,9 +0,0 @@
-import classes from './LoginComman.Module.css';
-
-const ForgotPassword = () => {
-  return (
-    <div className={classes.Login}>ForgotPassword</div>
-  )
-}
-
-export default ForgotPassword
