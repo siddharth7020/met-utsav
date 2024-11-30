d927ce98f323d3733421ffeb0ad62db3e82263dc
siddharth7020 <sidh1583@gmail.com>
2024-11-30 15:33:35 +0530

    Notice done

--- src/components/Common/NoticeForm.jsx
+++ src/components/Common/NoticeForm.jsx
@@ -1,8 +1,64 @@
+import PropTypes from 'prop-types';
 
-const NoticeForm = () => {
+const NoticeForm = ({setShowNoticeForm}) => {
+  NoticeForm.propTypes = {
+    setShowNoticeForm: PropTypes.func.isRequired,
+  };
+  
   return (
-    <div>NoticeForm</div>
+    <div className="flex justify-center items-center ">
+      <form className="w-full max-w-lg bg-white rounded-lg shadow-md p-6">
+        <div className="mb-4">
+          <label htmlFor="event" className="block text-gray-700 font-semibold mb-2">
+            Select Institute
+          </label>
+          <select
+            id="event"
+            className="w-full border border-gray-300 rounded-lg px-3 py-2 focus:ring-2 focus:ring-black focus:outline-none"
+          >
+            <option value="">Select Institute</option>
+            <option value="">Event 1</option>
+            <option value="">Event 2</option>
+            <option value="">Event 3</option>
+            <option value="">Event 4</option>
+            <option value="">Event 5</option>
+          </select>
+        </div>
+
+        <div className="mb-4">
+          <input
+            id="title"
+            type="text"
+            placeholder="Enter Title"
+            className="w-full border border-gray-300 rounded-lg px-3 py-2 focus:ring-2 focus:ring-black focus:outline-none"
+          />
+        </div>
+
+        <textarea
+          id="description"
+          placeholder="Enter Description"
+          rows="5"
+          className="w-full border border-gray-300 rounded-lg px-3 py-2 focus:ring-2 focus:ring-black focus:outline-none"
+        />
+
+        <div className="flex justify-end space-x-4">
+          <button
+            type="submit"
+            className="px-6 py-2 text-white bg-gradient-to-r from-red-500 to-red-700 hover:from-red-700 hover:to-red-900 text-sm text-white font-bold rounded-xl transition duration-200 "
+          >
+            Submit
+          </button>
+          <button
+            type="button"
+            className="px-6 py-2 bg-red-100 hover:bg-red-200 text-sm text-gray-900 font-bold  rounded-xl transition duration-200"
+            onClick={() => setShowNoticeForm(false)}
+          >
+            Cancel
+          </button>
+        </div>
+      </form>
+    </div>
   )
 }
 
-export default NoticeForm
+export default NoticeForm;
--- src/components/Tabs/NoticeTab.jsx
+++ src/components/Tabs/NoticeTab.jsx
@@ -1,15 +1,44 @@
-import NoticeCard from "../Common/NoticeCard"
+import { useState } from "react";
+import NoticeCard from "../Common/NoticeCard";
+import NoticeForm from "../Common/NoticeForm";
 
 
 const NoticeTab = () => {
+  const [showNoticeForm, setShowNoticeForm] = useState(false);
+
+  const handleNoticeButtonClick = () => {
+    setShowNoticeForm(true);
+  }
+  
   return (
     <div className="bg-gray-300 h-full ">
       <div className="max-w-7xl mx-auto px-4 py-12">
         <div className="flex flex-row justify-between mb-2">
           <h2 className="text-2xl font-bold mb-2">Student Attendance</h2>
-          <button className="lg:inline-block py-2 px-6 bg-gradient-to-r from-red-500 to-red-700 hover:from-red-700 hover:to-red-900 text-sm text-white font-bold rounded-xl transition duration-200" >Notice</button>
+          <button
+            onClick={handleNoticeButtonClick}
+            className="flex gap-2 text-white px-6 py-2 rounded-lg bg-gradient-to-r from-red-500 to-red-700 hover:from-red-700 hover:to-red-900 transition-colors"
+          >
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="20"
+              height="20"
+              fill="currentColor"
+              className="bi bi-plus"
+              viewBox="0 0 16 16"
+            >
+              <path d="M8 4a.5.5 0 0 1 .5.5v3h3a.5.5 0 0 1 0 1h-3v3a.5.5 0 0 1-1 0v-3h-3a.5.5 0 0 1 0-1h3v-3A.5.5 0 0 1 8 4" />
+            </svg>
+            Event
+          </button>
         </div>
-        <NoticeCard />
+
+        {
+          showNoticeForm && (
+              <NoticeForm setShowNoticeForm={setShowNoticeForm}/>
+          )
+        }
+          <NoticeCard />
       </div>
     </div>
   )
