db032ce0981ca5cefd1d90d6bb6f82337cfc4710
siddharth7020 <sidh1583@gmail.com>
2024-12-02 17:30:55 +0530

    serach incomplete

--- src/components/Common/AtdTable.jsx
+++ src/components/Common/AtdTable.jsx
@@ -1,8 +1,11 @@
 import { useState } from "react";
+import PropTypes from 'prop-types';
 
-
-const AtdTable = () => {
-    const Data = [
+AtdTable.propTypes = {
+    searchTerm: PropTypes.string.isRequired,
+  };
+const AtdTable = ({ searchTerm }) => {
+    const data = [
         {
             id: 1,
             name: "John Doe",
@@ -30,11 +33,11 @@ const AtdTable = () => {
         {
             id: 4,
             name: "Bob Brown",
-            RollNo: "1204", 
-            status: "Absent",   
+            RollNo: "1204",
+            status: "Absent",
             institute: "GHI Institute",
             eventName: "Chemistry",
-        },    
+        },
         {
             id: 5,
             name: "John Doe",
@@ -62,8 +65,8 @@ const AtdTable = () => {
         {
             id: 8,
             name: "Bob Brown",
-            RollNo: "1204", 
-            status: "Absent",   
+            RollNo: "1204",
+            status: "Absent",
             institute: "GHI Institute",
             eventName: "Chemistry",
         },
@@ -94,8 +97,8 @@ const AtdTable = () => {
         {
             id: 12,
             name: "Bob Brown",
-            RollNo: "1204", 
-            status: "Absent",   
+            RollNo: "1204",
+            status: "Absent",
             institute: "GHI Institute",
             eventName: "Chemistry",
         },
@@ -107,12 +110,12 @@ const AtdTable = () => {
     const rowsPerPage = 10;
 
     // Calculate total pages
-    const totalPages = Math.ceil(Data.length / rowsPerPage);
+    const totalPages = Math.ceil(data.length / rowsPerPage);
 
     // Get current rows to display
     const indexOfLastRow = currentPage * rowsPerPage;
     const indexOfFirstRow = indexOfLastRow - rowsPerPage;
-    const currentRows = Data.slice(indexOfFirstRow, indexOfLastRow);
+    const currentRows = data.slice(indexOfFirstRow, indexOfLastRow);
 
     // Handle pagination
     const handleNextPage = () => {
@@ -127,65 +130,17 @@ const AtdTable = () => {
         }
     };
 
+
+    const filteredData = data.filter((item) =>
+        item.name.toLowerCase().includes(searchTerm.toLowerCase())
+    );
+
+
     return (
-        // <div className="max-w-7xl mx-auto px-4 py-6">
-        //     <h2 className="text-2xl font-bold mb-5">Student Attendance</h2>
-        //     <div className="relative overflow-x-auto shadow-md sm:rounded-lg">
-        //         <table className="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
-        //             <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-white">
-        //                 <tr>
-        //                     <th scope="col" className="px-6 py-3">
-        //                         Attendance
-        //                     </th>
-        //                     <th scope="col" className="px-6 py-3">
-        //                         Name
-        //                     </th>
-        //                     <th scope="col" className="px-6 py-3">
-        //                         Institute
-        //                     </th>
-        //                     <th scope="col" className="px-6 py-3">
-        //                         Email
-        //                     </th>
-        //                     <th scope="col" className="px-6 py-3">
-        //                         Event Name
-        //                     </th>
-        //                 </tr>
-        //             </thead>
-        //             {
-        //                 Data.map((data, index) => (
-        //                     <tbody key={data.id || index}>
-        //                         <tr className="bg-white border-b   text-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600 dark:hover:text-white">
-        //                             <td className="w-4 p-4">
-        //                                 <div className="flex items-center">
-        //                                     <input id="checkbox-table-search-1" type="checkbox" className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 dark:focus:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600" />
-        //                                     <label htmlFor="checkbox-table-search-1" className="sr-only">checkbox</label>
-        //                                 </div>
-        //                             </td>
-        //                             <th scope="row" className="px-6 py-4 ">
-        //                                 {data.name}
-        //                             </th>
-        //                             <td className="px-6 py-4">
-        //                                 {data.institute}
-        //                             </td>
-        //                             <td className="px-6 py-4">
-        //                                 {data.email}
-        //                             </td>
-        //                             <td className="px-6 py-4">
-        //                                 {data.eventName}
-        //                             </td>
-        //                         </tr>
-        //                     </tbody>
-        //                 ))
-        //             }
 
-        //         </table>
-        //     </div>
-
-        // </div>
-        
-           
+        <>
             <div className="relative overflow-hidden shadow-md sm:rounded-lg">
-                <div className="h-100 ">
+                <div className="overflow-x-auto"> {/* Add overflow-x-auto here */}
                     <table className="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
                         <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-white">
                             <tr>
@@ -197,53 +152,76 @@ const AtdTable = () => {
                             </tr>
                         </thead>
                         <tbody>
-                            {currentRows.map((data) => (
-                                <tr
-                                    key={data.id}
-                                    className="bg-white border-b text-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600 dark:hover:text-white"
+                            {filteredData.length > 0 ? (
+                                currentRows.map((data) => (
+                                    <tr
+                                        key={data.id}
+                                        className="bg-white border-b text-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600 dark:hover:text-white"
+                                    >
+                                        <td className="w-4 p-4">
+                                            <div className="flex items-center">
+                                                <input
+                                                    id={`checkbox-${data.id}`}
+                                                    type="checkbox"
+                                                    className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 dark:focus:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"
+                                                />
+                                                <label htmlFor={`checkbox-${data.id}`} className="sr-only">
+                                                    checkbox
+                                                </label>
+                                            </div>
+                                        </td>
+                                        <th scope="row" className="px-6 py-4">{data.name}</th>
+                                        <td className="px-6 py-4">{data.institute}</td>
+                                        <td className="px-6 py-4">{data.RollNo}</td>
+                                        <td className="px-6 py-4">{data.eventName}</td>
+                                    </tr>
+                                ))
+                            ) : (
+                                <tr>
+                                <td
+                                  colSpan="3"
+                                  className="border border-gray-400 px-4 py-2 text-center"
                                 >
-                                    <td className="w-4 p-4">
-                                        <div className="flex items-center">
-                                            <input
-                                                id={`checkbox-${data.id}`}
-                                                type="checkbox"
-                                                className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 dark:focus:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"
-                                            />
-                                            <label
-                                                htmlFor={`checkbox-${data.id}`}
-                                                className="sr-only"
-                                            >
-                                                checkbox
-                                            </label>
-                                        </div>
-                                    </td>
-                                    <th scope="row" className="px-6 py-4">{data.name}</th>
-                                    <td className="px-6 py-4">{data.institute}</td>
-                                    <td className="px-6 py-4">{data.RollNo}</td>
-                                    <td className="px-6 py-4">{data.eventName}</td>
-                                </tr>
-                            ))}
+                                  No results found
+                                </td>
+                              </tr>
+                            )}
                         </tbody>
                     </table>
                 </div>
-                <div className="flex justify-between items-center mt-4">
+                <div className="flex justify-between items-center mt-4 flex-wrap">
                     <button
                         onClick={handlePreviousPage}
                         disabled={currentPage === 1}
-                        className={`px-4 py-2 text-white bg-blue-500 rounded ${currentPage === 1 ? "opacity-50 cursor-not-allowed" : "hover:bg-blue-600"}`}
+                        className={`px-4 py-2 text-white bg-red-500 rounded ${currentPage === 1 ? "opacity-50 cursor-not-allowed" : "hover:bg-red-600"}`}
                     >
                         Previous
                     </button>
-                    <span className="text-gray-700">Page {currentPage} of {totalPages}</span>
+                    <span className="text-gray-700 mt-2 lg:mt-0">
+                        Page {currentPage} of {totalPages}
+                    </span>
                     <button
                         onClick={handleNextPage}
                         disabled={currentPage === totalPages}
-                        className={`px-4 py-2 text-white bg-blue-500 rounded ${currentPage === totalPages ? "opacity-50 cursor-not-allowed" : "hover:bg-blue-600"}`}
+                        className={`px-4 py-2 text-white bg-red-500 rounded ${currentPage === totalPages ? "opacity-50 cursor-not-allowed" : "hover:bg-red-600"}`}
                     >
                         Next
                     </button>
                 </div>
             </div>
+
+            {/* Save Button */}
+            <div>
+                <button
+                    type="submit"
+                    className="flex items-center mt-2 px-6 py-2 text-white bg-gradient-to-r from-red-500 to-red-700 hover:from-red-700 hover:to-red-900 text-sm font-bold rounded-xl transition duration-200 w-full sm:w-auto"
+                >
+                    Save Attendance
+                </button>
+            </div>
+        </>
+
+
     )
 }
 
--- src/components/Tabs/AttendanceTab.jsx
+++ src/components/Tabs/AttendanceTab.jsx
@@ -4,7 +4,9 @@ import AtdTable from "../Common/AtdTable"
 
 const AttendanceTab = () => {
   const [filteropen, setFilterOpen] = useState(false);
+  const [searchTerm, setSearchTerm] = useState("");
 
+  
   const handleFilterButtonClick = () => {
     setFilterOpen(true); // Show the filter form
   };
@@ -13,7 +15,7 @@ const AttendanceTab = () => {
     setFilterOpen(false);
   };
   return (
-    <div className="bg-gray-100 h-full">
+    <div className="bg-gray-200 h-full">
       <div className="max-w-7xl mx-auto px-4 py-12">
         <div className="flex flex-row justify-between mb-2">
           <h2 className="text-2xl font-bold mb-2">Student Attendance</h2>
@@ -22,40 +24,80 @@ const AttendanceTab = () => {
         {
           filteropen && (
             <div className="flex flex-col bg-white rounded-lg p-4 shadow-sm mb-2">
-              <div className=" flex flex-row justify-between space-x-2">
-                <div className=" flex flex-row space-x-2">
-                  <div className="flex-1">
-                    <label className="text-black font-bold" htmlFor="country">Intitute</label>
-                    <select className="w-full bg-gray-100 rounded-md border-gray-300 text-black px-2 py-1" id="country">
-                      <option value="">Select a Intitute</option>
-                      <option value="AF">Institute Of asian management development City</option>
-                      <option value="DZ">ICS</option>
-                      <option value="AO">IOM  </option>
-                    </select>
-                  </div>
-
-                  <div className="flex-1">
-                    <label className="text-black font-bold" htmlFor="country">Events</label>
-
-                    <select className="w-full bg-gray-100 rounded-md border-gray-300 text-black px-2 py-1" id="country">
-                      <option value="">Select a Event</option>
-                      <option value="AF">Dance</option>
-                      <option value="DZ">Singing</option>
-                      <option value="AO">Music</option>
-                    </select>
-                  </div>
-                </div>
-
-                <div className="mt-4 flex flex-col  lg:flex-row lg:space-x-2">
-                  <button className="lg:inline-block lg:ml-auto lg:mr-3 py-2 px-6 bg-gradient-to-r from-red-500 to-red-700 hover:from-red-700 hover:to-red-900 text-sm text-white font-bold rounded-xl transition duration-200" type="submit">Submit</button>
-                  <button className="lg:inline-block lg:ml-auto lg:mr-3 py-2 px-6 bg-red-100 hover:bg-red-200 text-sm text-gray-900 font-bold  rounded-xl transition duration-200" onClick={handleCloseForm}>Close</button>
-                </div>
+            <div className="flex flex-col lg:flex-row lg:space-x-4 mb-4">
+              {/* Search Input */}
+              <div className="flex-1 mb-4 lg:mb-0">
+                <label className="text-black font-bold" htmlFor="search">
+                  Search
+                </label>
+                <input
+                  type="text"
+                  id="search"
+                  placeholder="Search..."
+                  className="w-full bg-gray-100 rounded-md border-gray-300 text-black px-2 py-1"
+                  value={searchTerm}
+                  onChange={(e) => setSearchTerm(e.target.value)}
+                />
               </div>
-
+          
+              {/* Institute Dropdown */}
+              <div className="flex-1 mb-4 lg:mb-0">
+                <label className="text-black font-bold" htmlFor="institute">
+                  Institute
+                </label>
+                <select
+                  className="w-full bg-gray-100 rounded-md border-gray-300 text-black px-2 py-1"
+                  id="institute"
+                >
+                  <option value="">Select an Institute</option>
+                  <option value="AF">
+                    Institute Of Asian Management Development City
+                  </option>
+                  <option value="DZ">ICS</option>
+                  <option value="AO">IOM</option>
+                </select>
+              </div>
+          
+              {/* Events Dropdown */}
+              <div className="flex-1 mb-4 lg:mb-0">
+                <label className="text-black font-bold" htmlFor="event">
+                  Events
+                </label>
+                <select
+                  className="w-full bg-gray-100 rounded-md border-gray-300 text-black px-2 py-1"
+                  id="event"
+                >
+                  <option value="">Select an Event</option>
+                  <option value="AF">Dance</option>
+                  <option value="DZ">Singing</option>
+                  <option value="AO">Music</option>
+                </select>
+              </div>
             </div>
+          
+            {/* Buttons */}
+            <div className="flex flex-col justify-center lg:flex-row lg:space-x-2">
+              <button
+                className="lg:inline-block lg:mr-3 py-2 px-6 bg-gradient-to-r from-red-500 to-red-700 hover:from-red-700 hover:to-red-900 text-sm text-white font-bold rounded-xl transition duration-200"
+                type="submit"
+              >
+                Submit
+              </button>
+              <button
+                className="lg:inline-block  lg:mr-3 py-2 px-6 bg-red-100 hover:bg-red-200 text-sm text-gray-900 font-bold rounded-xl transition duration-200"
+                onClick={handleCloseForm}
+              >
+                Close
+              </button>
+            </div>
+          </div>
+          
+          
           )
         }
-        <AtdTable />
+        <div>
+          <AtdTable searchTerm={searchTerm}/>
+        </div>
       </div>
     </div>
   )
