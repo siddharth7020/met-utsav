07ed46f21ccf1df05d3f9ce49f0601a8d55970d1
siddharth7020 <sidh1583@gmail.com>
2024-12-03 12:57:14 +0530

    role table changes

--- src/components/Tabs/AttendanceTab.jsx
+++ src/components/Tabs/AttendanceTab.jsx
@@ -6,7 +6,7 @@ const AttendanceTab = () => {
   const [filteropen, setFilterOpen] = useState(false);
   const [searchTerm, setSearchTerm] = useState("");
 
-  
+
   const handleFilterButtonClick = () => {
     setFilterOpen(true); // Show the filter form
   };
@@ -24,79 +24,79 @@ const AttendanceTab = () => {
         {
           filteropen && (
             <div className="flex flex-col bg-white rounded-lg p-4 shadow-sm mb-2">
-            <div className="flex flex-col lg:flex-row lg:space-x-4 mb-4">
-              {/* Search Input */}
-              <div className="flex-1 mb-4 lg:mb-0">
-                <label className="text-black font-bold" htmlFor="search">
-                  Search
-                </label>
-                <input
-                  type="text"
-                  id="search"
-                  placeholder="Search..."
-                  className="w-full bg-gray-100 rounded-md border-gray-300 text-black px-2 py-1"
-                  value={searchTerm}
-                  onChange={(e) => setSearchTerm(e.target.value)}
-                />
+              <div className="flex flex-col lg:flex-row lg:space-x-4 mb-4">
+                {/* Search Input */}
+                <div className="flex-1 mb-4 lg:mb-0">
+                  <label className="text-black font-bold" htmlFor="search">
+                    Search
+                  </label>
+                  <input
+                    type="text"
+                    id="search"
+                    placeholder="Search..."
+                    className="w-full bg-gray-100 rounded-md border-gray-300 text-black px-2 py-1"
+                    value={searchTerm}
+                    onChange={(e) => setSearchTerm(e.target.value)}
+                  />
+                </div>
+
+                {/* Institute Dropdown */}
+                <div className="flex-1 mb-4 lg:mb-0">
+                  <label className="text-black font-bold" htmlFor="institute">
+                    Institute
+                  </label>
+                  <select
+                    className="w-full bg-gray-100 rounded-md border-gray-300 text-black px-2 py-1"
+                    id="institute"
+                  >
+                    <option value="">Select an Institute</option>
+                    <option value="AF">
+                      Institute Of Asian Management Development City
+                    </option>
+                    <option value="DZ">ICS</option>
+                    <option value="AO">IOM</option>
+                  </select>
+                </div>
+
+                {/* Events Dropdown */}
+                <div className="flex-1 mb-4 lg:mb-0">
+                  <label className="text-black font-bold" htmlFor="event">
+                    Events
+                  </label>
+                  <select
+                    className="w-full bg-gray-100 rounded-md border-gray-300 text-black px-2 py-1"
+                    id="event"
+                  >
+                    <option value="">Select an Event</option>
+                    <option value="AF">Dance</option>
+                    <option value="DZ">Singing</option>
+                    <option value="AO">Music</option>
+                  </select>
+                </div>
               </div>
-          
-              {/* Institute Dropdown */}
-              <div className="flex-1 mb-4 lg:mb-0">
-                <label className="text-black font-bold" htmlFor="institute">
-                  Institute
-                </label>
-                <select
-                  className="w-full bg-gray-100 rounded-md border-gray-300 text-black px-2 py-1"
-                  id="institute"
+
+              {/* Buttons */}
+              <div className="flex flex-col justify-center lg:flex-row lg:space-x-2">
+                <button
+                  className="lg:inline-block lg:mr-3 py-2 px-6 bg-gradient-to-r from-red-500 to-red-700 hover:from-red-700 hover:to-red-900 text-sm text-white font-bold rounded-xl transition duration-200"
+                  type="submit"
                 >
-                  <option value="">Select an Institute</option>
-                  <option value="AF">
-                    Institute Of Asian Management Development City
-                  </option>
-                  <option value="DZ">ICS</option>
-                  <option value="AO">IOM</option>
-                </select>
-              </div>
-          
-              {/* Events Dropdown */}
-              <div className="flex-1 mb-4 lg:mb-0">
-                <label className="text-black font-bold" htmlFor="event">
-                  Events
-                </label>
-                <select
-                  className="w-full bg-gray-100 rounded-md border-gray-300 text-black px-2 py-1"
-                  id="event"
+                  Submit
+                </button>
+                <button
+                  className="lg:inline-block  lg:mr-3 py-2 px-6 bg-red-100 hover:bg-red-200 text-sm text-gray-900 font-bold rounded-xl transition duration-200"
+                  onClick={handleCloseForm}
                 >
-                  <option value="">Select an Event</option>
-                  <option value="AF">Dance</option>
-                  <option value="DZ">Singing</option>
-                  <option value="AO">Music</option>
-                </select>
+                  Close
+                </button>
               </div>
             </div>
-          
-            {/* Buttons */}
-            <div className="flex flex-col justify-center lg:flex-row lg:space-x-2">
-              <button
-                className="lg:inline-block lg:mr-3 py-2 px-6 bg-gradient-to-r from-red-500 to-red-700 hover:from-red-700 hover:to-red-900 text-sm text-white font-bold rounded-xl transition duration-200"
-                type="submit"
-              >
-                Submit
-              </button>
-              <button
-                className="lg:inline-block  lg:mr-3 py-2 px-6 bg-red-100 hover:bg-red-200 text-sm text-gray-900 font-bold rounded-xl transition duration-200"
-                onClick={handleCloseForm}
-              >
-                Close
-              </button>
-            </div>
-          </div>
-          
-          
+
+
           )
         }
         <div>
-          <AtdTable searchTerm={searchTerm}/>
+          <AtdTable searchTerm={searchTerm} />
         </div>
       </div>
     </div>
--- src/components/Tabs/RolesTab.jsx
+++ src/components/Tabs/RolesTab.jsx
@@ -1,13 +1,25 @@
 import { useState } from "react";
-import RolesTabComponents from "../Common/RolesTabComponents";
 import Table from "../Common/Table";
 
 const RolesTab = () => {
+
+  const [filteropen, setFilterOpen] = useState(false);
+  const [searchTerm, setSearchTerm] = useState("");
+
+
+  const handleFilterButtonClick = () => {
+    setFilterOpen(true); // Show the filter form
+  };
+
+  const handleCloseForm = () => {
+    setFilterOpen(false);
+  };
+
   const columns = [
     { field: "checkBox", header: "Select" },
     { field: "name", header: "Name" },
     { field: "role", header: "Role" },
-    { field: "status", header: "Status" },
+    { field: "status", header: "Category Status" },
     { field: "institute", header: "Institute" },
   ];
 
@@ -63,7 +75,7 @@ const RolesTab = () => {
     { id: 49, checkBox: <input type="checkbox" />, name: "Sofia Johnson", role: "Teacher", status: "Singing", institute: "GHI" },
     { id: 50, checkBox: <input type="checkbox" />, name: "Gabriel Clark", role: "Admin", status: "Dance", institute: "ICS" },
   ];
-  
+
 
   const [currentPage, setCurrentPage] = useState(1);
   const rowsPerPage = 10;
@@ -92,7 +104,84 @@ const RolesTab = () => {
   return (
     <div className="bg-gray-300 h-full">
       <div className="max-w-7xl mx-auto px-4 py-12">
-        <RolesTabComponents />
+        <div className="flex flex-row justify-between mb-2">
+          <h2 className="text-2xl font-bold mb-2">Roles</h2>
+          <button className="lg:inline-block py-2 px-6 bg-gradient-to-r from-red-500 to-red-700 hover:from-red-700 hover:to-red-900 text-sm text-white font-bold rounded-xl transition duration-200" onClick={handleFilterButtonClick}>Filter</button>
+        </div>
+        {
+          filteropen && (
+            <div className="flex flex-col bg-white rounded-lg p-4 shadow-sm mb-2">
+              <div className="flex flex-col lg:flex-row lg:space-x-4 mb-4">
+                {/* Search Input */}
+                <div className="flex-1 mb-4 lg:mb-0">
+                  <label className="text-black font-bold" htmlFor="search">
+                    Search
+                  </label>
+                  <input
+                    type="text"
+                    id="search"
+                    placeholder="Search..."
+                    className="w-full bg-gray-100 rounded-md border-gray-300 text-black px-2 py-1"
+                    value={searchTerm}
+                    onChange={(e) => setSearchTerm(e.target.value)}
+                  />
+                </div>
+
+                {/* Institute Dropdown */}
+                <div className="flex-1 mb-4 lg:mb-0">
+                  <label className="text-black font-bold" htmlFor="institute">
+                    Institute
+                  </label>
+                  <select
+                    className="w-full bg-gray-100 rounded-md border-gray-300 text-black px-2 py-1"
+                    id="institute"
+                  >
+                    <option value="">Select an Institute</option>
+                    <option value="AF">
+                      ICS
+                    </option>
+                    <option value="DZ">ITC</option>
+                    <option value="AO">ABC</option>
+                  </select>
+                </div>
+
+                {/* Roles Dropdown */}
+                <div className="flex-1 mb-4 lg:mb-0">
+                  <label className="text-black font-bold" htmlFor="event">
+                    Roles
+                  </label>
+                  <select
+                    className="w-full bg-gray-100 rounded-md border-gray-300 text-black px-2 py-1"
+                    id="event"
+                  >
+                    <option value="">Select an Roles</option>
+                    <option value="AF">Student</option>
+                    <option value="DZ">Volunteer</option>
+                    <option value="AO">Coordinator</option>
+                  </select>
+                </div>
+              </div>
+
+              {/* Buttons */}
+              <div className="flex flex-col justify-center lg:flex-row lg:space-x-2">
+                <button
+                  className="lg:inline-block lg:mr-3 py-2 px-6 bg-gradient-to-r from-red-500 to-red-700 hover:from-red-700 hover:to-red-900 text-sm text-white font-bold rounded-xl transition duration-200"
+                  type="submit"
+                >
+                  Submit
+                </button>
+                <button
+                  className="lg:inline-block  lg:mr-3 py-2 px-6 bg-red-100 hover:bg-red-200 text-sm text-gray-900 font-bold rounded-xl transition duration-200"
+                  onClick={handleCloseForm}
+                >
+                  Close
+                </button>
+              </div>
+            </div>
+
+
+          )
+        }
         <div className="relative overflow-hidden shadow-md sm:rounded-lg">
           {/* Pass currentRows to the Table */}
           <Table columns={columns} data={currentRows} />
@@ -100,11 +189,10 @@ const RolesTab = () => {
             <button
               onClick={handlePreviousPage}
               disabled={currentPage === 1}
-              className={`px-4 py-2 text-white bg-red-500 rounded ${
-                currentPage === 1
+              className={`px-4 py-2 text-white bg-red-500 rounded ${currentPage === 1
                   ? "opacity-50 cursor-not-allowed"
                   : "hover:bg-red-600"
-              }`}
+                }`}
             >
               Previous
             </button>
@@ -114,11 +202,10 @@ const RolesTab = () => {
             <button
               onClick={handleNextPage}
               disabled={currentPage === totalPages}
-              className={`px-4 py-2 text-white bg-red-500 rounded ${
-                currentPage === totalPages
+              className={`px-4 py-2 text-white bg-red-500 rounded ${currentPage === totalPages
                   ? "opacity-50 cursor-not-allowed"
                   : "hover:bg-red-600"
-              }`}
+                }`}
             >
               Next
             </button>
