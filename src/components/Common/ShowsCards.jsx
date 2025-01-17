55bc06a5c2cc958fde7e35b9bf898477f4fd5cf4
Harshad Zagade <Hzagade21@gmail.com>
2025-01-17 14:27:59 +0530

    Update button in ShowsCards component to change text and color for registration

--- src/components/Common/ShowsCards.jsx
+++ src/components/Common/ShowsCards.jsx
@@ -67,8 +67,8 @@ const ShowsCards = () => {
                     </div>
                     <div className="flex items-center justify-between p-4">
                         <div className="flex items-center">
-                            <button className='bg-gray-400 text-white px-4 py-2 rounded-md' >
-                                Coming Soon
+                            <button className='bg-red-600 text-white px-4 py-2 rounded-md' onClick={() => handleClick('/workshop')}>
+                                Register Here
                             </button>
                         </div>
                     </div>
